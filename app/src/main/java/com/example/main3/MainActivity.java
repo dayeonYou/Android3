3a88ebdffc3a1b3d2aa2ba5427c768e814d2a6bd
dayeonYou <0620dadada@suwon.ac.kr>
2022-09-29 17:09:47 +0900

    main3

--- app/src/main/java/com/example/main3/MainActivity.java
+++ app/src/main/java/com/example/main3/MainActivity.java
@@ -7,8 +7,10 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.content.Intent;
+import android.os.Vibrator;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -72,8 +74,7 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<String> contentTextArray = new ArrayList<>();
         //서버에서 openCV로 추출한 텍스르 가져와서 저장
         String contentText;
-        String recipient;
-        String address;
+        String recipient,address,waybill;
         contentTextArray.add("운송장_1234567890 받으시는 분_홍*동 주소_경기도 화성시");
         contentText = contentTextArray.get(0);
         boolean b = contentText.contains("운송장");
@@ -81,13 +82,37 @@ public class MainActivity extends AppCompatActivity {
         TextView mainText2 = (TextView) findViewById(R.id.yesParcel);
         TextView waybillText = (TextView)findViewById(R.id.waybillT);
         TextView addressText = (TextView) findViewById(R.id.addressT);
-        TextView recipientText = (TextView) findViewById(R.id.addressT);
+        TextView recipientText = (TextView) findViewById(R.id.recipientT);
+
         if(b){ //받은 택배 있음
             mainText1.setVisibility(View.GONE);
             mainText2.setVisibility(View.VISIBLE);
-            address = contentText.replaceAll("[^0-9]","");
+            waybill = contentText.replaceAll("[^0-9]","");
+            waybillText.append(waybill);
+            recipient = returnString(contentText,"받으시는 분");
+            recipientText.append(recipient);
+            address = returnString(contentText,"주소");
             addressText.append(address);
-            //recipient =
+            boolean parcelYN;
+
+            RadioButton rightParcelBtn = (RadioButton) findViewById(R.id.rightParcel);
+            rightParcelBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getApplicationContext(), "It will be processed as a received parcel. Thank you.", Toast.LENGTH_SHORT).show();
+                    Vibrator vib = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+                    vib.vibrate(1000);
+                }
+            });
+            RadioButton wrongParcelBtn = (RadioButton) findViewById(R.id.wrongParcel);
+            wrongParcelBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getApplicationContext(), "It is not your parcel.", Toast.LENGTH_SHORT).show();
+                    Vibrator vib = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+                    vib.vibrate(1000);
+                }
+            });
         }
         else{ //받은 택배 없음
             mainText1.setVisibility(View.VISIBLE);
@@ -105,6 +130,14 @@ public class MainActivity extends AppCompatActivity {
         ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
         return activeNetwork.getType();
+    }
+    private String returnString(String str,String wd){
+        String result = null;
+        for(int i=0;i<str.length();i++){
+            if(wd == "받으시는 분") result = str.split(wd)[1].split("주소")[0];
+            else result = str.split(wd)[1];
+        }
+        return result;
     }
 
 }
