5638fb67e286d2c6f642e9f5b07ad10f4688dbf1
dayeonYou <0620dadada@suwon.ac.kr>
2022-09-29 15:18:57 +0900

    main3

--- app/src/main/java/com/example/main3/MainActivity.java
+++ app/src/main/java/com/example/main3/MainActivity.java
@@ -11,7 +11,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
 import java.lang.*;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -65,15 +69,25 @@ public class MainActivity extends AppCompatActivity {
             default:
                 break;
         }
-        String contentText = "운송장있음";
-        int waybill = 12345;
+        ArrayList<String> contentTextArray = new ArrayList<>();
+        //서버에서 openCV로 추출한 텍스르 가져와서 저장
+        String contentText;
+        String recipient;
+        String address;
+        contentTextArray.add("운송장_1234567890 받으시는 분_홍*동 주소_경기도 화성시");
+        contentText = contentTextArray.get(0);
         boolean b = contentText.contains("운송장");
         TextView mainText1 = (TextView) findViewById(R.id.noParcel);
         TextView mainText2 = (TextView) findViewById(R.id.yesParcel);
+        TextView waybillText = (TextView)findViewById(R.id.waybillT);
+        TextView addressText = (TextView) findViewById(R.id.addressT);
+        TextView recipientText = (TextView) findViewById(R.id.addressT);
         if(b){ //받은 택배 있음
             mainText1.setVisibility(View.GONE);
             mainText2.setVisibility(View.VISIBLE);
-            mainText2.append("\n\n 택배 정보\n운송장 번호 : 1234556");
+            address = contentText.replaceAll("[^0-9]","");
+            addressText.append(address);
+            //recipient =
         }
         else{ //받은 택배 없음
             mainText1.setVisibility(View.VISIBLE);
